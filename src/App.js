c9ad60cbf80e4c6c7ec969342b2a3340c38082b2
Joshua Ramat <ramatjoshua@gmail.com>
2023-06-04 10:29:40 -0400

    completed Hero Component and setup file architecture

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <Box 
+      width="400px"
+      sx={{ width: { xl: '1488px'}}} 
+      m="auto"
+    >
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  )
+}
+
+export default App
